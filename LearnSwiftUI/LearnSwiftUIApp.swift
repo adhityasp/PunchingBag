2c8c782418a8818d3cf0ea9121df8cb50b3e9bc1
Tama <adhityasurya.pratama@paypay-corp.co.jp>
2023-01-14 10:00:13 +0700

    Initial Commit

---
+++ LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by adhityasurya.pratama on 2023/01/14.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
