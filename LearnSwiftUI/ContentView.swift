7bff6c6ff1dadca43b717edf13b3e9d845dbb4e2
Tama <tama.adhitya@paypay-corp.co.jp>
2024-08-02 20:39:32 +0900

    --wip-- [skip ci]

--- LearnSwiftUI/ContentView.swift
+++ LearnSwiftUI/ContentView.swift
@@ -1,26 +1,94 @@
-//
-//  ContentView.swift
-//  LearnSwiftUI
-//
-//  Created by adhityasurya.pratama on 2023/01/14.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var viewModel = BarcodeScreenViewModel()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(viewModel.barcodeResponse?.barcode ?? "")
+            Toggle("Use PayPay Points", isOn: $viewModel.isUsingPayPayPoints)
         }
-        .padding()
+        .task { await viewModel.binding() }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct BarcodeResponse: Equatable {
+    let barcode: String
+}
+
+protocol BarcodeRepository {
+    func getBarcode() async throws -> BarcodeResponse
+    func toggleUsingPayPayPoints(_ isUsingPayPayPoints: Bool) async throws
+}
+
+struct DefaultBarcodeRepository: BarcodeRepository {
+
+    static var counter = 0
+    func getBarcode() async throws -> BarcodeResponse {
+        try await Task.sleep(nanoseconds: UInt64.random(in: 1_000_000_000...2_000_000_000))
+        DefaultBarcodeRepository.counter += 1
+        return BarcodeResponse(barcode: "Barcode \(DefaultBarcodeRepository.counter)")
+    }
+
+    func toggleUsingPayPayPoints(_ isUsingPayPayPoints: Bool) async throws {
+        try await Task.sleep(nanoseconds: UInt64.random(in: 0...1_000_000_000))
+    }
+}
+
+@MainActor
+final class BarcodeScreenViewModel: ObservableObject {
+
+    @Published private(set) var barcodeResponse: BarcodeResponse?
+    @Published private(set) var isLoadingBarcode = false
+    @Published var isUsingPayPayPoints = false
+
+    private let barcodeRepository: BarcodeRepository
+
+    init(barcodeRepository: BarcodeRepository = DefaultBarcodeRepository()) {
+        self.barcodeRepository = barcodeRepository
+    }
+
+    private func getBarcode() async {
+        isLoadingBarcode = true
+        defer { isLoadingBarcode = false }
+        do {
+            barcodeResponse = try await barcodeRepository.getBarcode()
+        } catch {
+            // handle error
+        }
+    }
+
+    private func toggleUsingPayPayPoints(_ isUsingPayPayPoints: Bool) async {
+        do {
+            try await barcodeRepository.toggleUsingPayPayPoints(isUsingPayPayPoints)
+        } catch {
+            // handle error
+        }
+        await getBarcode()
+    }
+
+    func binding() async {
+        await withTaskGroup(of: Void.self) {
+            $0.addTask {
+                await self.getBarcode()
+            }
+
+            $0.addTask {
+                let isUsingPayPayPoints = await self.$isUsingPayPayPoints
+                    .dropFirst()
+                    .removeDuplicates()
+                    .values
+
+                for await isUsingPayPayPoints in isUsingPayPayPoints {
+                    await self.toggleUsingPayPayPoints(isUsingPayPayPoints)
+                }
+            }
+        }
     }
 }
---
+++ LearnSwiftUITests/LearnSwiftUITests.swift
@@ -0,0 +1,97 @@
+// Created by Tama Adhitya on 2024/08/02
+
+import XCTest
+import Combine
+import ConcurrencyExtras
+
+@testable import LearnSwiftUI
+
+final class LearnSwiftUITests: XCTestCase {
+
+    @MainActor
+    func testBinding_shouldGetBarcodeResponse() async {
+        await withMainSerialExecutor {
+            let mockBarcodeRepository = MockBarcodeRepository()
+            let response = BarcodeResponse(barcode: "barcode")
+            mockBarcodeRepository.result = .success(response)
+
+            let sut = BarcodeScreenViewModel(barcodeRepository: mockBarcodeRepository)
+
+            await sut.binding()
+            await Task.yield()
+            sut.isUsingPayPayPoints = true
+
+            XCTAssertEqual(sut.barcodeResponse, BarcodeResponse(barcode: "barcode"))
+            XCTAssertEqual(mockBarcodeRepository.getBarcodeCallCount, 2)
+            XCTAssertEqual(mockBarcodeRepository.toggle, 1)
+        }
+    }
+
+    @MainActor
+    func testBinding_shouldChangeLoadingBarcodeState() async {
+        let mockBarcodeRepository = MockBarcodeRepository()
+        let response = BarcodeResponse(barcode: "barcode")
+        mockBarcodeRepository.result = .success(response)
+        let sut = BarcodeScreenViewModel(barcodeRepository: mockBarcodeRepository)
+
+        let task = Task { await sut.binding() }
+
+        await Task.yield()
+        await Task.yield()
+        await Task.yield()
+        await Task.yield()
+        await Task.yield()
+        await Task.yield()
+        XCTAssertTrue(sut.isLoadingBarcode)
+
+        await task.value
+        XCTAssertFalse(sut.isLoadingBarcode)
+    }
+
+    @MainActor
+    func testBinding_whenIsUsingPayPayPointsChanged_shouldGetBarcode() async {
+        let mockBarcodeRepository = MockBarcodeRepository()
+        let response = BarcodeResponse(barcode: "barcode")
+        mockBarcodeRepository.result = .success(response)
+
+        let sut = BarcodeScreenViewModel(barcodeRepository: mockBarcodeRepository)
+
+        await sut.binding()
+        XCTAssertFalse(sut.isUsingPayPayPoints)
+        XCTAssertEqual(mockBarcodeRepository.getBarcodeCallCount, 1)
+
+        sut.isUsingPayPayPoints = true
+        XCTAssertEqual(mockBarcodeRepository.getBarcodeCallCount, 2)
+
+        sut.isUsingPayPayPoints = false
+        XCTAssertEqual(mockBarcodeRepository.getBarcodeCallCount, 3)
+    }
+}
+
+class MockBarcodeRepository: BarcodeRepository {
+    var result: Result<BarcodeResponse, Error> = .failure(ErrorInTest.dummy)
+    var getBarcodeCompletion: (() -> BarcodeResponse)?
+    private(set) var getBarcodeCallCount = 0
+
+    private(set) var toggle = 0
+
+    func getBarcode() async throws -> BarcodeResponse {
+        getBarcodeCallCount += 1
+
+        getBarcodeCompletion?()
+        switch result {
+        case .success(let success):
+            return success
+        case .failure(let failure):
+            throw failure
+        }
+    }
+
+    func toggleUsingPayPayPoints(_ isUsingPayPayPoints: Bool) async throws {
+        toggle += 1
+    }
+}
+
+enum ErrorInTest: Error {
+    case dummy
+}
